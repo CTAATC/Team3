9d47a09c5a6e231581d2b1d0f130124676d5939d
CTAATC <164135952+CTAATC@users.noreply.github.com>
2024-06-09 11:09:53 +0800

    Update Sudoku.cpp

--- 數獨/數獨/Sudoku.cpp
+++ 數獨/數獨/Sudoku.cpp
@@ -21,6 +21,18 @@ Sudoku::Sudoku(int version) {
         maze[0][1] = maze[2][3] = maze[3][2] = maze[5][5] = '8';
         maze[0][8] = maze[1][2] = maze[3][5] = maze[5][7] = maze[7][6] = maze[8][3] = '9';
     }
+        
+    else if (version == 1) {
+    maze[0][8] = maze[1][2] = maze[3][5] = maze[4][1] = maze[7][7] = '1';
+    maze[3][1] = maze[5][4] = maze[8][0] = '2';
+    maze[0][7] = maze[1][1] = maze[5][2] = maze[6][4] = '3';
+    maze[0][5] = maze[2][1] = maze[4][0] = maze[6][6] = maze[7][4] = '4';
+    maze[0][6] = maze[2][0] = maze[3][3] = '5';
+    maze[1][6] = maze[4][7] = maze[5][1] = maze[6][8] = maze[8][5] = '6';
+    maze[1][5] = maze[2][8] = maze[3][7] = maze[7][3] = maze[8][6] = '7';
+    maze[1][0] = maze[2][6] = maze[8][1] = '8';
+    maze[1][8] = maze[2][2] = maze[4][6] = maze[6][7] = maze[7][1] = '9';
+    }
 }
 
 char Sudoku::mazeAnswer(int ver, int m_r, int m_c) {
@@ -34,6 +46,18 @@ char Sudoku::mazeAnswer(int ver, int m_r, int m_c) {
         }
 
         return answer[m_r][m_c];
+    }
+
+    if (ver == 1) {
+        int a = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                answer[i][j] = answer2[a];
+                a += 1;
+            }
+        }
+
+    return answer[m_r][m_c];
     }
 }
 
