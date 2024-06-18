a199108ef3b7c60d0ea11d07725c6759c6d87ce6
Weper021 <93174276+Weper021@users.noreply.github.com>
2024-06-18 23:00:55 +0800

    Update Sudoku.cpp

--- 數獨/數獨/Sudoku.cpp
+++ 數獨/數獨/Sudoku.cpp
@@ -2,15 +2,14 @@
 #include <iostream>
 using namespace std;
 
-//從主程式尋找版本放上數獨的初始值
 Sudoku::Sudoku(int version) {
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
             maze[i][j] = '.';
         }
     }
-        
-    
+
+
     if (version == 0) {
         maze[1][4] = maze[5][3] = maze[7][5] = '1';
         maze[2][7] = maze[4][2] = maze[5][4] = maze[6][1] = '2';
@@ -22,21 +21,20 @@ Sudoku::Sudoku(int version) {
         maze[0][1] = maze[2][3] = maze[3][2] = maze[5][5] = '8';
         maze[0][8] = maze[1][2] = maze[3][5] = maze[5][7] = maze[7][6] = maze[8][3] = '9';
     }
-        
+
     else if (version == 1) {
-    maze[0][8] = maze[1][2] = maze[3][5] = maze[4][1] = maze[7][7] = '1';
-    maze[3][1] = maze[5][4] = maze[8][0] = '2';
-    maze[0][7] = maze[1][1] = maze[5][2] = maze[6][4] = '3';
-    maze[0][5] = maze[2][1] = maze[4][0] = maze[6][6] = maze[7][4] = '4';
-    maze[0][6] = maze[2][0] = maze[3][3] = '5';
-    maze[1][6] = maze[4][7] = maze[5][1] = maze[6][8] = maze[8][5] = '6';
-    maze[1][5] = maze[2][8] = maze[3][7] = maze[7][3] = maze[8][6] = '7';
-    maze[1][0] = maze[2][6] = maze[8][1] = '8';
-    maze[1][8] = maze[2][2] = maze[4][6] = maze[6][7] = maze[7][1] = '9';
+        maze[0][8] = maze[1][2] = maze[3][5] = maze[4][1] = maze[7][7] = '1';
+        maze[3][1] = maze[5][4] = maze[8][0] = '2';
+        maze[0][7] = maze[1][1] = maze[5][2] = maze[6][4] = '3';
+        maze[0][5] = maze[2][1] = maze[4][0] = maze[6][6] = maze[7][4] = '4';
+        maze[0][6] = maze[2][0] = maze[3][3] = '5';
+        maze[1][6] = maze[4][7] = maze[5][1] = maze[6][8] = maze[8][5] = '6';
+        maze[1][5] = maze[2][8] = maze[3][7] = maze[7][3] = maze[8][6] = '7';
+        maze[1][0] = maze[2][6] = maze[8][1] = '8';
+        maze[1][8] = maze[2][2] = maze[4][6] = maze[6][7] = maze[7][1] = '9';
     }
 }
 
-//放進版本、第幾排、列並傳回該位置的答案
 char Sudoku::mazeAnswer(int ver, int m_r, int m_c) {
     if (ver == 0) {
         int a = 0;
@@ -59,21 +57,16 @@ char Sudoku::mazeAnswer(int ver, int m_r, int m_c) {
             }
         }
 
-    return answer[m_r][m_c];
+        return answer[m_r][m_c];
     }
 }
 
-//把輸入的正確答案放進數獨格子中
 void Sudoku::setMaze(int row, int column, char input) {
     maze[row][column] = input;
 }
-
-//回傳數獨中該位置的數值
 char Sudoku::getMazeValue(int row, int column) {
     return maze[row][column];
 }
-
-//輸出目前的數獨狀況
 void Sudoku::print() {
 
     int a = 0;
@@ -89,10 +82,9 @@ void Sudoku::print() {
         cout << endl;
         if (i % 3 == 2) cout << " -------------" << endl;
     }
-    cout << "請輸入三個數字 [第幾橫排] [第幾直排] [要輸入的數字] 或輸入 'h' 來獲取提示" << endl;
+    cout << "請輸入三個數字 [直排] [橫排] [要輸入的數字] 或輸入 'h' 來獲取提示" << endl;
 }
 
-//確認是否還有未完成輸入的格子
 bool Sudoku::mazeCheck() {
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
