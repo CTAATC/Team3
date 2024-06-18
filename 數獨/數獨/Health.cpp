c81c3d0bb953f8d038c33bb68493c69f923a1c32
CTAATC <164135952+CTAATC@users.noreply.github.com>
2024-06-18 21:44:19 +0800

    Update Health.cpp

--- 數獨/數獨/Health.cpp
+++ 數獨/數獨/Health.cpp
@@ -2,16 +2,18 @@
 #include <iostream>
 using namespace std;
 
-
+//傳回生命值
 int Health::getHealth() {
 	return health;
 }
 
+//寫出目前的生命值
 void Health::printHealth(){
 	cout << "\nYou have " << health << " lives left." << endl;
 	cout << endl;
 }
 
+//扣一條生命值
 int Health::decreaseHealth() {
 	health = health - 1;
 	return  health;
